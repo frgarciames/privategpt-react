6605680fbc0964f246c664211f14625b313bd173
frgarciames <fran17901@gmail.com>
2024-05-08 16:18:42 +0200

    fix: routing when privategpt instance is not working

--- src/pages/root.tsx
+++ src/pages/root.tsx
@@ -6,10 +6,9 @@ import { useLocalStorage } from 'usehooks-ts';
 
 export const RootPage = () => {
   const navigate = useNavigate();
-  const [environment, setEnvironment] = useLocalStorage<string | undefined>(
-    'pgpt-url',
-    undefined,
-  );
+  const [environment, , deleteEnvironment] = useLocalStorage<
+    string | undefined
+  >('pgpt-url', undefined);
 
   useEffect(() => {
     if (!environment) {
@@ -18,16 +17,20 @@ export const RootPage = () => {
         'http://localhost:8001',
       );
       if (!url) return;
-      checkIsPgptHealthy(url).then((isHealthy) => {
-        if (isHealthy) {
-          setEnvironment(url);
-        } else {
-          alert('The provided URL is not a healthy Private GPT instance');
-        }
-      });
     }
-    navigate('/chat');
-  }, []);
+  }, [environment]);
+
+  useEffect(() => {
+    if (!environment) return;
+    checkIsPgptHealthy(environment).then((isHealthy) => {
+      if (!isHealthy) {
+        alert('The Private GPT instance is not healthy');
+        return deleteEnvironment();
+      }
+      navigate('/chat');
+    });
+  }, [environment]);
+
   if (environment) return <Outlet />;
   return <div>Loading...</div>;
 };
